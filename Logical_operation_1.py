91e8a88df22a3fbcd53c293238d9b4e4c201123c
YOGEESH K P <yogeesh793@gmail.com>
2024-09-09 20:01:51 +0530

    Add files via upload

---
+++ Logical_operation_1.py
@@ -0,0 +1,18 @@
+# Python Logical Operator
+A=False
+B=True
+
+# Logical AND Operator
+print(A and B)
+print(A and A)
+print(B and B)
+
+# Logical OR Operator
+print(A or B)
+print(A or A)
+print(B or B)
+
+# Logical NOT Operator
+print (not(A and B))
+print(not(A and A))
+print(not(B and B))
